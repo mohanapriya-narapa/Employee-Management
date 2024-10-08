74d25ee5da3db4392b118d7a231eb1b4550f9eb6
mohanapriya-narapa <mohanapriya.narapa@outlook.com>
2024-10-07 20:55:13 -0500

    add button

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const employeeList=document.querySelector("#employeeList");
 let employees = [];
 window.onload= async function(){
-    
-  
+    const addEmployeeBtn=document.querySelector("#addEmployeeBtn");
+    const employeeModel=document.querySelector("#employeeModel");
+    const closeModalBtn=document.querySelector("#closeModalBtn");
+    const employeeForm=document.querySelector("#employeeForm");
     try{
 const response=await axios.get("data.json");
 employees=response.data;
@@ -11,7 +13,27 @@ renderEmployees();
     catch{
 console.log("error loading data");
     }
-
+    addEmployeeBtn.addEventListener("click", ()=>{
+        employeeModel.style.display="flex";
+    })
+    closeModalBtn.addEventListener("click",()=>{
+        console.log("clicked");
+        
+        employeeModel.style.display="none";
+    })
+    employeeForm.addEventListener("submit", (e)=>{
+        e.preventDefault();
+        const newEmployee={
+            id:employees.length+1,
+            firstName:document.getElementById("firstName").value,
+            lastName: document.getElementById("lastName").value
+        }
+        employees.push(newEmployee);
+        renderEmployees();
+        employeeModel.style.display = "none";
+        employeeForm.reset()
+    })
+   
 }
 function renderEmployees(){
     employeeList.innerHTML="";
